e4f8e946b5c41642b713e1dcaa49e873ed1fd41d
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-04 19:06:34 -0300

    Payment, one-to-one association

--- src/main/java/com/thiagopolli/course/config/TestConfg.java
+++ src/main/java/com/thiagopolli/course/config/TestConfg.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.thiagopolli.course.entities.Category;
 import com.thiagopolli.course.entities.Order;
 import com.thiagopolli.course.entities.OrderItem;
+import com.thiagopolli.course.entities.Payment;
 import com.thiagopolli.course.entities.Product;
 import com.thiagopolli.course.entities.User;
 import com.thiagopolli.course.entities.enums.OrderStatus;
@@ -91,6 +92,10 @@ public class TestConfg implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
