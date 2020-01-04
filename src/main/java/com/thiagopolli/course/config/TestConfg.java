f25669c9771f08a53b2c92395b28bda6166321d5
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-04 15:33:55 -0300

    Category

--- src/main/java/com/thiagopolli/course/config/TestConfg.java
+++ src/main/java/com/thiagopolli/course/config/TestConfg.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.thiagopolli.course.entities.Category;
 import com.thiagopolli.course.entities.Order;
 import com.thiagopolli.course.entities.User;
 import com.thiagopolli.course.entities.enums.OrderStatus;
+import com.thiagopolli.course.repositories.CategoryRepository;
 import com.thiagopolli.course.repositories.OrderRepository;
 import com.thiagopolli.course.repositories.UserRepository;
 
@@ -24,9 +26,17 @@ public class TestConfg implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -37,8 +47,13 @@ public class TestConfg implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WATING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WATING_PAYMENT, u1);
 		
+		
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		
 	}
 	
