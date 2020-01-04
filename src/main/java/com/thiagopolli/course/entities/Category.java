db18eeacda9b33594a4020c46597d70a461cad1e
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-04 16:49:43 -0300

    Product

--- src/main/java/com/thiagopolli/course/entities/Category.java
+++ src/main/java/com/thiagopolli/course/entities/Category.java
@@ -8,31 +8,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
-public class Category  implements Serializable{
-	
-	
+public class Category implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@Transient
-	private Set<Product> products = new HashSet<>();
-	
-	
-	
-	public Category() {
-		
-	}
 
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
+	public Category() {
+
+	}
 
 	public Category(Long id, String name) {
 		super();
@@ -40,31 +39,25 @@ public class Category  implements Serializable{
 		this.name = name;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 
 	public Set<Product> getProducts() {
 		return products;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -73,7 +66,6 @@ public class Category  implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -91,9 +83,5 @@ public class Category  implements Serializable{
 			return false;
 		return true;
 	}
-
-
-	
-	
 
 }
