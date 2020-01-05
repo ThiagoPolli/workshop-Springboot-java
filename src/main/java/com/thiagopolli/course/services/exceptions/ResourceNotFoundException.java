ed4881aaa7de19383cc351c923862e7bc5f3f2ca
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-04 21:36:59 -0300

    Exception handling - findById

---
+++ src/main/java/com/thiagopolli/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.thiagopolli.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource Not Found Id " + id);
+	}
+		
+}
