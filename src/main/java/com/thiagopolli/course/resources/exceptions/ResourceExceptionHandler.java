c5c752288338c2c702a142e832b1c226bd17cb87
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-04 21:56:41 -0300

    Exception handling - delete

--- src/main/java/com/thiagopolli/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/thiagopolli/course/resources/exceptions/ResourceExceptionHandler.java
@@ -21,5 +21,15 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 
 }
